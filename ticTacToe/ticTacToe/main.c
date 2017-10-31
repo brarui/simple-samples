ccb32ce40edcb33f06305320b8e333bb233cb969
Rui Silva <bra.ui@gmail.com>
2017-10-31 12:16:35 +0000

    Git initialization

---
+++ ticTacToe/ticTacToe/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ticTacToe
+//
+//  Created by Rui Silva on 31/10/17.
+//  Copyright © 2017 Rui Silva. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
